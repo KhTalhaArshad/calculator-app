fe925258dd211135daa903e63612f16d292b27e9
KhTalhaArshad <126275156+KhTalhaArshad@users.noreply.github.com>
2023-05-28 05:00:45 -0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,8 +1,3 @@
-from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
 
 class myApp(App):
     def build(self):
